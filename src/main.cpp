6cc1866db5c90fc9d265caf515b09a91719c6d73
CosmicIon <harshnivedan@gmail.com>
2026-03-13 00:25:22 +0530

    input move as string

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include "board.h"
+#include "defs.h"
+#include "input.h"
+#include <iostream>
+#include <string>
 
 int main() {
     initBoard();
-    printBoard();
+    bool whiteToMove = true;
+
+    while (true) {
+        system("cls"); //to clear the terminal screen.
+        printBoard();
+
+        std::cout << (whiteToMove ? "White" : "Black") << " to move: ";
+        std::string input;
+        std::cin >> input;
+
+        if (input == "quit") {
+            break;
+        }
+
+        Move move;
+        if (!parseMove(input, move)) {
+            continue; //if input is invalid, skips the loop.
+        }
+
+        //check source square has a piece
+        if (board[move.srcRank][move.srcFile] == EMPTY) {
+            std::cout << "No piece at that square. Press Enter to continue." << std::endl;
+            std::cin.get(); std::cin.get(); //this will consume the Enter we press. that way user get a chance to read the error message.
+            continue;
+        }
+
+        //making the move .. without checking any rule for now.
+        board[move.dstRank][move.dstFile] = board[move.srcRank][move.srcFile];
+        board[move.srcRank][move.srcFile] = EMPTY;
+
+        whiteToMove = !whiteToMove;
+    }
+
     return 0;
 }
